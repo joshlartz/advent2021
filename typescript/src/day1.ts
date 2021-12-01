dcfb2b38dafd99c183c0d9e57ddee136fb5db4f3
Josh Lartz <JLartz@cvent.com>
2021-12-01 14:18:19 -0600

    day 1 part 2

--- typescript/src/day1.ts
+++ typescript/src/day1.ts
@@ -2,12 +2,30 @@ import readInput from './readInput';
 
 const input = readInput('1').map(x => parseInt(x));
 
-if (process.argv[2] == '1') {
-  const solution = input.reduce((acc, depth, index, array) => {
+const reduce = depths =>
+  depths.reduce((acc, depth, index, array) => {
     // console.log({ depth }, { index }, (depth > array[index-1]) ? 1 : 0);
-    acc += (depth > array[index-1]) ? 1 : 0;
+    acc += depth > array[index - 1] ? 1 : 0;
     return acc;
   }, 0);
+
+if (process.argv[2] == '1') {
+  const solution = reduce(input);
+
+  console.log(solution);
+}
+
+if (process.argv[2] == '2') {
+  const threeSums = input
+    .map((depth, index, array) => {
+      // console.log({ depth }, { index }, depth + array[index + 1] + array[index + 2]);
+      if (index + 2 < array.length - 1) return depth + array[index + 1] + array[index + 2];
+    })
+    .filter(x => x);
+
+  // console.log(threeSums);
+
+  const solution = reduce(threeSums);
 
   console.log(solution);
 }
