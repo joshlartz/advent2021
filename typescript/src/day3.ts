6c9413abb20194f66b741aceb21993171b102a36
Josh Lartz <JLartz@cvent.com>
2021-12-02 23:51:16 -0600

    better part 1

--- typescript/src/day3.ts
+++ typescript/src/day3.ts
@@ -1,13 +1,13 @@
 import readInput from './readInput';
 
-const input = readInput('3').map(binary => binary.split('').map(char => parseInt(char)));
+const input = readInput('3');
 
 if (process.argv[2] == '1') {
   const columnCounts = new Array(input[0].length).fill(undefined).map(x => ({ zero: 0, one: 0 }));
 
   for (let i = 0; i < input[0].length; i++) {
     input.forEach(num => {
-      if (num[i]) columnCounts[i].one++;
+      if (parseInt(num[i])) columnCounts[i].one++;
       else columnCounts[i].zero++;
     });
   }
