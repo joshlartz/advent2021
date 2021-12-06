86daf7189a3001944b99fda159749fe87fbfbe8e
Josh Lartz <JLartz@cvent.com>
2021-12-05 21:40:11 -0600

    rust day 1 part 1

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,9 +1,46 @@
-use std::iter;
+// use std::iter;
 use std::num::ParseIntError;
 
-use itertools::izip;
+use aoc_runner_derive::{aoc, aoc_generator};
+// use itertools::izip;
 
 #[aoc_generator(day1)]
 pub fn generator(input: &str) -> Result<Vec<u32>, ParseIntError> {
     input.lines().map(str::parse).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[u32]) -> i32 {
+    return input.iter().enumerate().fold(0, |acc, (index, depth)| {
+        acc + (depth > &input[if index == 0 { 0 } else { index - 1 }]) as i32
+    });
+}
+
+// #[aoc(day1, part2)]
+// pub fn part2(input: Vec<u32>) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &str = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(&generator(SAMPLE).unwrap()), 7);
+    }
+
+    // #[test]
+    // fn sample2() {
+    //     assert_eq!(part2(&read_input(SAMPLE).unwrap()), 5);
+    // }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,3 @@
-extern crate aoc_runner;
-
-#[macro_use]
-extern crate aoc_runner_derive;
-
 pub mod day1;
 
-aoc_lib! { year = 2021 }
+aoc_runner_derive::aoc_lib! { year = 2021 }
