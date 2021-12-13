dd18ecb52955af8a0b5b3a819377b59cec3b5613
Josh Lartz <JLartz@cvent.com>
2021-12-13 16:27:14 -0600

    parse ASC-II letters back to strings

--- typescript/src/day13.ts
+++ typescript/src/day13.ts
@@ -6,8 +6,7 @@ type Grid = number[][];
 function part1(input: Input) {
   let grid = createGrid(input);
 
-  fold(grid, input.folds[0])
-  // print(grid);
+  fold(grid, input.folds[0]);
   console.log(sum(grid));
 }
 
@@ -15,8 +14,22 @@ function part2(input: Input) {
   let grid = createGrid(input);
 
   input.folds.forEach(f => fold(grid, f));
-  print(grid);
-  // console.log(sum(grid));
+  // print(grid);
+
+  // sample case
+  if (grid[0].length === 5) console.log(letter(grid.slice(0, 5)));
+  // go the extra mile and convert the characters to a string for programmatic submission
+  else
+    console.log(
+      letter(grid.map(y => y.filter((_, x) => x >= 0 && x < 4))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 5 && x < 9))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 10 && x < 14))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 15 && x < 19))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 20 && x < 24))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 25 && x < 29))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 30 && x < 34))) +
+        letter(grid.map(y => y.filter((_, x) => x >= 35 && x < 39)))
+    );
 }
 
 function sum(grid: Grid) {
@@ -69,7 +82,31 @@ function parseInput(input: string[]) {
 }
 
 function print(grid: Grid) {
-  grid.forEach(row => console.log(row.map(x => (x === 1 ? '#' : '.')).join('')));
+  grid.forEach(row => console.log(row.map(x => (x === 1 ? '#' : ' ')).join('')));
+}
+
+// prettier-ignore
+function letter(letter: Grid) {
+  const letters: Map<string, string> = new Map();
+  const serialize = grid => JSON.stringify(grid);
+
+  letters.set(serialize([[0, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 1], [1, 0, 0, 1], [1, 0, 0, 1]]), 'A');
+  letters.set(serialize([[1, 1, 1, 0], [1, 0, 0, 1], [1, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 0]]), 'B');
+  letters.set(serialize([[0, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 1], [0, 1, 1, 0]]), 'C');
+  letters.set(serialize([[1, 1, 1, 1], [1, 0, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 1]]), 'E');
+  letters.set(serialize([[1, 1, 1, 1], [1, 0, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]), 'F');
+  letters.set(serialize([[0, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 0], [1, 0, 1, 1], [1, 0, 0, 1], [0, 1, 1, 1]]), 'G');
+  letters.set(serialize([[1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1]]), 'H');
+  letters.set(serialize([[0, 0, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0]]), 'J');
+  letters.set(serialize([[1, 0, 0, 1], [1, 0, 1, 0], [1, 1, 0, 0], [1, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1]]), 'K');
+  letters.set(serialize([[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 1]]), 'L');
+  letters.set(serialize([[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]]), 'O');
+  letters.set(serialize([[1, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 0], [1, 0, 0, 0], [1, 0, 0, 0]]), 'P');
+  letters.set(serialize([[1, 1, 1, 0], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1]]), 'R');
+  letters.set(serialize([[1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 1, 0]]), 'U');
+  letters.set(serialize([[1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0], [1, 1, 1, 1]]), 'Z');
+  
+  return letters.get(serialize(letter));
 }
 
 if (require.main === module) {
@@ -117,9 +154,9 @@ fold along x=5`.split('\n');
     expect(logSpy).toBeCalledWith(17);
   });
 
-  test('part2 sample2', () => {
+  test('part2', () => {
     const logSpy = mockLog();
     part2(parseInput(sample));
-    expect(logSpy).toBeCalledWith(16);
+    expect(logSpy).toBeCalledWith('O');
   });
 }
